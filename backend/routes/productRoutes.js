c907f37bdb5156d7e77b8c7bb622c6eed89f77e5
ArunJena <arunjena632@gmail.com>
2024-03-29 17:03:00 +0530

    backend: added controller to make the routes file more structured

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,6 +1,7 @@
 import express from "express";
 import asyncHandler from "../middleware/asyncHandler.js";
 import Product from "../models/productModel.js";
+import { getProductsById } from "../controllers/productController.js";
 
 const router = express.Router();
 
@@ -12,17 +13,6 @@ router.get(
   })
 );
 
-router.get(
-  "/:id",
-  asyncHandler(async (req, res) => {
-    const product = await Product.findById(req.params.id);
-    if (product) {
-      res.json(product);
-    } else {
-      res.status(404);
-      throw new Error("Resource not found");
-    }
-  })
-);
+router.route("/:id").get(getProductsById);
 
 export default router;

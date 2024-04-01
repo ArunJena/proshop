d2a265cc0c31915394c1d688b5118aa6d93e90cc
ArunJena <arunjena632@gmail.com>
2024-04-01 23:35:25 +0530

    frontend: added redux and redux-toolkit, implemented apiSlice to handle the fetch and related states, change in the productRoutes in backend

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,17 +1,12 @@
 import express from "express";
-import asyncHandler from "../middleware/asyncHandler.js";
-import Product from "../models/productModel.js";
-import { getProductsById } from "../controllers/productController.js";
+import {
+  getProductsById,
+  getProducts,
+} from "../controllers/productController.js";
 
 const router = express.Router();
 
-router.get(
-  "/",
-  asyncHandler(async (req, res) => {
-    const products = await Product.find({});
-    res.json(products);
-  })
-);
+router.route("/").get(getProducts);
 
 router.route("/:id").get(getProductsById);
 

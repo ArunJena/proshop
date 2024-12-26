e58f360fbd37ed58ed8917dbc0d41c5a9632b63d
arun <arunjena632@gmail.com>
2024-12-26 07:23:36 +0530

    backend: added user routes and controllers

---
+++ backend/controllers/userController.js
@@ -0,0 +1,77 @@
+import asyncHandler from "../middleware/asyncHandler.js";
+import User from "../models/userModel.js";
+
+//@desc Auth user & get tocken
+//@route POST /api/users/login
+//@access Public
+const authUser = asyncHandler(async (req, res) => {
+  res.send("auth user");
+});
+
+//@desc Register user
+//@route POST /api/users
+//@access Public
+const registerUser = asyncHandler(async (req, res) => {
+  res.send("register user");
+});
+
+//@desc Logout user / clear cookie
+//@route POST /api/users/logout
+//@access Private
+const logoutUser = asyncHandler(async (req, res) => {
+  res.send("logout user");
+});
+
+//@desc Get user profile
+//@route POST /api/users/profile
+//@access Public
+const getUserProfile = asyncHandler(async (req, res) => {
+  res.send("getUserProfiler");
+});
+
+//@desc Update user profile
+//@route PUT /api/users/profile
+//@access Private
+const updateUserProfile = asyncHandler(async (req, res) => {
+  res.send("updateUserProfile");
+});
+
+//@desc Get all users
+//@route GET /api/users
+//@access Private/Admin
+const getUsers = asyncHandler(async (req, res) => {
+  res.send("getUsers");
+});
+
+//@desc Delete user
+//@route DELETE /api/users/:id
+//@access Private/Admin
+const deleteUser = asyncHandler(async (req, res) => {
+  res.send("deleteUser");
+});
+
+//@desc Get user by ID
+//@route GET /api/users/:id
+//@access Private/Admin
+const getUserById = asyncHandler(async (req, res) => {
+  res.send("getUserById");
+});
+
+//@desc Update user
+//@route PUT /api/users/:id
+//@access Private/Admin
+const updateUser = asyncHandler(async (req, res) => {
+  res.send("updateUser");
+});
+
+export {
+  authUser,
+  registerUser,
+  logoutUser,
+  getUserProfile,
+  updateUserProfile,
+  getUsers,
+  deleteUser,
+  getUserById,
+  updateUser,
+};

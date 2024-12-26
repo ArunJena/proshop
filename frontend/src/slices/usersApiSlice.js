d04928e163425180328e790830925b00b40400c3
arun <arunjena632@gmail.com>
2024-12-26 11:01:37 +0530

    frontend: added authSlice and userApiSlice

---
+++ frontend/src/slices/usersApiSlice.js
@@ -0,0 +1,17 @@
+import { USER_URL } from "../constants";
+import { apiSlice } from "./apiSlice";
+
+export const usersApiSlice = apiSlice.injectEndpoints({
+  endpoints: (builder) => ({
+    login: builder.mutation({
+      query: (data) => ({
+        url: USER_URL / auth,
+        method: "POST",
+        body: data,
+      }),
+      keepUnusedDataFor: 5,
+    }),
+  }),
+});
+
+export const { useLoginMutation } = usersApiSlice;

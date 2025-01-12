777ab779dbc88a81666989277f53b8856195d894
arun <arunjena632@gmail.com>
2025-01-12 18:11:43 +0530

    frontend: added ProductListScreen

---
+++ frontend/src/screens/admin/ProductListScreen.jsx
@@ -0,0 +1,77 @@
+import { LinkContainer } from "react-router-bootstrap";
+import { Table, Button, Row, Col } from "react-bootstrap";
+import { FaTimes, FaEdit, FaTrash, FaPlus } from "react-icons/fa";
+import Message from "../../components/Message";
+import Loader from "../../components/Loader";
+import { useGetProductsQuery } from "../../slices/productsApiSlice";
+
+const ProductListScreen = () => {
+  const { data: products, error, isLoading } = useGetProductsQuery();
+  const deleteHandler = (id) => {
+    if (window.confirm("Are you sure?")) {
+      // DELETE PRODUCT
+    }
+  };
+  return (
+    <>
+      <Row className="align-items-center">
+        <Col>
+          <h1>Products</h1>
+        </Col>
+        <Col className="text-right">
+          <LinkContainer to="/admin/product/create">
+            <Button className="my-3">
+              <FaPlus /> Create Product
+            </Button>
+          </LinkContainer>
+        </Col>
+      </Row>
+      {isLoading ? (
+        <Loader />
+      ) : error ? (
+        <Message variant="danger">{error.error}</Message>
+      ) : (
+        <Table striped bordered hover responsive className="table-sm">
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>NAME</th>
+              <th>PRICE</th>
+              <th>CATEGORY</th>
+              <th>BRAND</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {products.map((product) => (
+              <tr key={product._id}>
+                <td>{product._id}</td>
+                <td>{product.name}</td>
+                <td>${product.price}</td>
+                <td>{product.category}</td>
+                <td>{product.brand}</td>
+                <td>
+                  <LinkContainer to={`/admin/product/${product._id}/edit`}>
+                    <Button variant="light" className="btn-sm">
+                      <FaEdit />
+                    </Button>
+                  </LinkContainer>
+                  <Button
+                    style={{ color: "white" }}
+                    variant="danger"
+                    className="btn-sm"
+                    onClick={() => deleteHandler(product._id)}
+                  >
+                    <FaTrash />
+                  </Button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+      )}
+    </>
+  );
+};
+
+export default ProductListScreen;
